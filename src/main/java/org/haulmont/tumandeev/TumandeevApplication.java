82c1dc70659755d2239f80c373f209ac9cce1f3c
GreyWay2 <68636141+GreyWay2@users.noreply.github.com>
2021-08-08 17:46:07 +0300

    init

---
+++ src/main/java/org/haulmont/tumandeev/TumandeevApplication.java
@@ -0,0 +1,13 @@
+package org.haulmont.tumandeev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TumandeevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TumandeevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/haulmont/tumandeev/TumandeevApplicationTests.java
@@ -0,0 +1,13 @@
+package org.haulmont.tumandeev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TumandeevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
